a2c634dbb21ccf3136681271a4def4b1665412a8
Andy Hong <chili.tomat0@icloud.com>
2024-03-06 14:58:21 +0900

    240306A

---
+++ BJ/main.cpp
@@ -0,0 +1,15 @@
+//
+//  #####
+//  문제이름
+//
+//  Created by 홍준우 on M/D/YY.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
--- BJ2525 - 오븐 시계/main.cpp
+++ BJ2525 - 오븐 시계/main.cpp
@@ -13,9 +13,7 @@ int main() {
     int a, b;
     int c;
     int min;
-    cin >> a;
-    cin >> b;
-    cin >> c;
+    cin >> a >> b >> c;
     min = 60 * a + b + c;
     int result_hr, result_min;
     result_hr = (int) (min/60);
@@ -24,7 +22,7 @@ int main() {
     // Counter: result_hr가 24보다 클 경우
     if ( result_hr > 24 )
         result_hr -= 24;
-    result_min = min%60;
+    result_min = min % 60;
     cout << result_hr << " " << result_min << endl;
     
     return 0;
