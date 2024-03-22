c17c3c6c0146b05fbfdea6995509dc63e6ce6089
Andy Hong <chili.tomat0@icloud.com>
2024-03-23 02:05:02 +0900

    Update main.cpp

--- BJ1110 - 더하기 사이클/main.cpp
+++ BJ1110 - 더하기 사이클/main.cpp
@@ -18,13 +18,8 @@ int main() {
     k = n;
     
     do {
-        if (k < 10) {
-            sum = k;
-            k = 11 * k;
-        } else {
-            sum = k / 10 + k % 10;
-            k = (k % 10) * 10 + sum % 10;
-        }
+        sum = k / 10 + k % 10;
+        k = (k % 10) * 10 + sum % 10;
         i++;
     } while (n != k);
     cout << i << endl;
